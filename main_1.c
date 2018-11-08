352dd97529509da1372739ad42e58a9239078235
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-11-09 01:00:22 +0900

    배열의 활용 2

--- main_1.c
+++ main_1.c
@@ -8,15 +8,14 @@ int main(int argc, char *argv[]) {
 	int i;
 	int grade[5];
 	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
+	for(i=0; i<5; i++){
+		printf("input value (%d) : ", i);
+		scanf("%d", &grade[i]);
+	}
 	
-	i = 3;
-	
-	printf("grade[%d] = %d\n", i, grade[i]);
+	for(i=0; i<5; i++){
+		printf("grade[%d] = %d\n", i, grade[i]);
+	}
 	
 	return 0;
 }
